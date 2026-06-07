f77d226a4e4a1ce97622ebf7b4c85087491f4e2b
Onur <cndy121@gmail.com>
2026-06-07 16:40:02 +0300

    Add auth callback URL

--- koschei/api/public/js/koschei-auth.js
+++ koschei/api/public/js/koschei-auth.js
@@ -60,7 +60,7 @@ var KoscheiAuth = (function() {
     const res = await fetch(path, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ email, password, callbackURL: 'https://tradepigloball.co/hub.html' }),
     });
 
     const data = await res.json().catch(() => ({}));
