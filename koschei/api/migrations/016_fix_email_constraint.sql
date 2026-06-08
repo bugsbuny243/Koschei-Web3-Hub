e0e24528497159184913b1c8b93cfa56f49dd0cb
Onur <cndy121@gmail.com>
2026-06-08 11:23:59 +0300

    Delete koschei/api/migrations directory

--- koschei/api/migrations/016_fix_email_constraint.sql
+++
@@ -1,8 +0,0 @@
--- Drop unique constraint on email, keep unique on auth_subject
-ALTER TABLE app_user_profiles
-DROP CONSTRAINT IF EXISTS app_user_profiles_email_key;
-
-ALTER TABLE app_user_profiles
-DROP CONSTRAINT IF EXISTS app_user_profiles_email_unique;
-
-DROP INDEX IF EXISTS idx_app_user_profiles_email_unique;
