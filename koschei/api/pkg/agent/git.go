a3939661912b3472fe54b59e58fd6fe1a98c8569
Onur <cndy121@gmail.com>
2026-06-10 21:47:25 +0300

    Create git.go

---
+++ koschei/api/pkg/agent/git.go
@@ -0,0 +1,30 @@
+package agent
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func RunGitCommand(args ...string) error {
+	cmd := exec.Command("git", args...)
+	cmd.Dir = "."
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("git %v failed: %s", args, out)
+	}
+	return nil
+}
+
+func CreateBranch(branchName string) error {
+	return RunGitCommand("checkout", "-b", branchName)
+}
+
+func AddAndCommit(file, msg string) error {
+	RunGitCommand("add", file)
+	return RunGitCommand("commit", "-m", msg)
+}
+
+func PushBranch(branchName string) error {
+	return RunGitCommand("push", "origin", branchName)
+}
