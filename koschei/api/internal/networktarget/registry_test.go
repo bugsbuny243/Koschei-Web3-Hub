1d78b7b6bdba3450327185caa7195ab693350fec
Onur <cndy121@gmail.com>
2026-09-18 20:02:03 +0300

    style(arvis): gofmt expanded network registry test

--- koschei/api/internal/networktarget/registry_test.go
+++ koschei/api/internal/networktarget/registry_test.go
@@ -32,9 +32,9 @@ func TestAddressIdentityIsNetworkScoped(t *testing.T) {
 
 func TestCatalogReportsImplementedProbesWithoutClaimingLiveAvailability(t *testing.T) {
 	probeReady := map[string]bool{
-		"ethereum-mainnet": false,
-		"base-mainnet":     false,
-		"arbitrum-mainnet": false,
+		"ethereum-mainnet":  false,
+		"base-mainnet":      false,
+		"arbitrum-mainnet":  false,
 		"optimism-mainnet":  false,
 		"polygon-mainnet":   false,
 		"bnb-mainnet":       false,
