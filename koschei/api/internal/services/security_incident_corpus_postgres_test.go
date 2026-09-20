52c582c20c27a924df83093e18ec979b8f62b937
Onur <cndy121@gmail.com>
2026-09-20 21:51:22 +0300

    gofmt incident corpus auth fixture

--- koschei/api/internal/services/security_incident_corpus_postgres_test.go
+++ koschei/api/internal/services/security_incident_corpus_postgres_test.go
@@ -36,10 +36,10 @@ func TestSecurityIncidentCorpusPostgres17(t *testing.T) {
 	defer func() { _ = tx.Rollback() }()
 
 	const (
-		network  = "solana-mainnet"
-		targetA  = "IncidentCorpusTargetA1111111111111111111111111111"
-		actorA   = "IncidentCorpusActorA11111111111111111111111111111"
-		eventA   = "incident-corpus-event-signature-a"
+		network = "solana-mainnet"
+		targetA = "IncidentCorpusTargetA1111111111111111111111111111"
+		actorA  = "IncidentCorpusActorA11111111111111111111111111111"
+		eventA  = "incident-corpus-event-signature-a"
 		targetB = "IncidentCorpusTargetB1111111111111111111111111111"
 		actorB  = "IncidentCorpusActorB11111111111111111111111111111"
 		eventB  = "incident-corpus-event-signature-b"
